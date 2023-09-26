b05b062f83a9f5e34d7e45e2a7b78cbbaf53caab
Eldiyar1 <mc.oks1ne@gmail.com>
2023-09-26 18:18:08 +0600

    api/v1/

--- english_backend/urls.py
+++ english_backend/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.nza.urls'))
+    path('api/v1/', include('app.nza.urls'))
 ] + doc_urls
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
