d6929560b4003c6e82a1437577c8ce5597172d59
Eldiyar1 <mc.oks1ne@gmail.com>
2023-09-26 18:25:29 +0600

    media

--- english_backend/urls.py
+++ english_backend/urls.py
@@ -17,13 +17,8 @@ Including another URLconf
 from .settings.swagger import urlpatterns as doc_urls
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('app.nza.urls'))
 ] + doc_urls
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
